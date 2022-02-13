78495ab9c220adebaafa2719fd4ec62db4f0bc32
k15015kk <aitk15015kk@outlook.com>
2022-02-13 20:23:12 +0900

    地図を独自地図に変更

--- src/hooks/useMap.tsx
+++ src/hooks/useMap.tsx
@@ -12,35 +12,17 @@ const useMap = () => {
             return;
         }
 
+        console.log(process.env.NEXT_PUBLIC_MAP_URL)
+
         map = new maplibregl.Map({
             container: mapContainer.current,
-            style: {
-              "version": 8,
-              "sources": {
-                "photo": {
-                  "type": "raster",
-                  "tiles": ["https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg"],
-                  "tileSize": 256,
-                  "attribution": "<a href='http://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院地図</a>"
-                }
-              },
-              "layers": [
-                {
-                  "id": "base",
-                  "type": "raster",
-                  "source": "photo",
-                  "minzoom": 0,
-                  "maxzoom": 22
-                }
-              ]
-            },
+            style: process.env.NEXT_PUBLIC_MAP_URL || '',
             center: [139.77, 35.67],
             zoom: 11,
             bearing: 0,
-            pitch: 60
+            pitch: 0
         });
 
-        map.addControl(new maplibregl.NavigationControl());
     },[])
 
     return [mapContainer]
