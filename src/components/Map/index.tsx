957c1808076f6d1368c43d25a80d66e25b97127c
k15015kk <aitk15015kk@outlook.com>
2022-02-14 00:51:51 +0900

    千葉県をdeckgl上でレイヤーとして表示する部分を実装

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,13 +1,45 @@
-import useMap from "@/hooks/useMap";
+import ReactMapGL from 'react-map-gl'
+import { useState } from "react";
+import {DeckGL} from "deck.gl";
+import {GeoJsonLayer} from "@deck.gl/layers";
+import maplibregl from 'maplibre-gl';
 
 const Map = () => {
-    const [mapContainer] = useMap();
 
-    console.log(mapContainer)
+    const data = require('@/data/data.json')
+
+    const [viewport, setViewport] = useState({
+        width: 400,
+        height: 400,
+        latitude: 35.681236,
+        longitude: 139.767125,
+        zoom: 10
+    });
+
+    const layer = new GeoJsonLayer({
+        data,
+        filled: true,
+        stroked: true,
+        getLineWidth: 10,
+        getLineColor: [0, 0, 0],
+        getFillColor: [0, 64, 245, 60]
+    })
 
     return (
         <>
-            <div className="h-full" ref={mapContainer}/>
+        <DeckGL
+            width={"100vw"}
+            height={"100vh"}
+            controller
+            layers={[layer]}
+            viewState={viewport}
+            onViewStateChange={(viewState) => setViewport(viewState.viewState)}
+        >
+            <ReactMapGL
+                mapLib={maplibregl}
+                mapStyle={process.env.NEXT_PUBLIC_MAP_URL}
+            />
+        </DeckGL>
         </>
     );
 }
